273b8751007d806f21cbd5b70ab8a2cf9e067632
aviastro75 <aviastro75@gmail.com>
2025-03-18 21:19:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+incomes = [
+{"amount": 1000, "description": "Salary"},
+{"amount": 200, "description": "Freelance work"}
+]
+expenses = [
+{"amount": 500, "description": "Rent"},
+{"amount": 100, "description": "Utilities"}
+]
+
+
+while True:
+
+  print("Welcome to Budget Manager v1.0\n")
+
 
