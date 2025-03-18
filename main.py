aff3852263972ea8528ae5cc1375fabd596d9e6d
aviastro75 <aviastro75@gmail.com>
2025-03-18 21:23:48 +0200

    Added menu

--- main.py
+++ main.py
@@ -12,4 +12,10 @@ while True:
 
   print("Welcome to Budget Manager v1.0\n")
 
+  print("1. add new income")
+  print("2. add new expense")
+  print("3. show balance")
+  print("4. show transaction history")
+  print("5. exit")
+
 
