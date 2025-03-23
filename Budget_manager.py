87f017347f901d3c16536d2ccd586fbd885b5064
aviastro75 <aviastro75@gmail.com>
2025-03-23 20:18:41 +0200

    add expenses incomes and balance

--- Budget_manager.py
+++ Budget_manager.py
@@ -2,11 +2,14 @@ incomes = [
 {"amount": 1000, "description": "Salary"},
 {"amount": 200, "description": "Freelance work"}
 ]
+
 expenses = [
 {"amount": 500, "description": "Rent"},
 {"amount": 100, "description": "Utilities"}
 ]
 
+balance = sum(item["amount"] for item in incomes) - sum(item["amount"] for item in
+expenses)
 
 while True:
 
